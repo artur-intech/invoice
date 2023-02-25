6782f1e32dc5cd4198254917d8f635bad5cd3289
Artur Beljajev <artur@intech.ee>
2023-02-25 14:28:54 +0000

    Extract method

--- test/PgInvoicesTest.cs
+++ test/PgInvoicesTest.cs
@@ -1,3 +1,4 @@
+using System.Dynamic;
 using NUnit.Framework;
 
 namespace Intech.Invoice.Test;
@@ -27,22 +28,42 @@ class PgInvoicesTest : Base
                                                        clientId: client.Id).Id();
 
         Assert.AreEqual(1, pgDataSource.CreateCommand("SELECT COUNT(*) FROM invoices").ExecuteScalar());
-        var sql = "SELECT * FROM invoices WHERE id = $1";
-        using var command = pgDataSource.CreateCommand(sql);
-        command.Parameters.AddWithValue(createdInvoiceId);
+        dynamic dbRow = DbRow(createdInvoiceId);
+        Assert.AreEqual(number, dbRow.Number);
+        Assert.AreEqual(date, dbRow.Date);
+        Assert.AreEqual(dueDate, dbRow.DueDate);
+        Assert.AreEqual(vatRate, dbRow.VatRate);
+        Assert.AreEqual(client.Id, dbRow.ClientId);
+        Assert.AreEqual(supplier.Name, dbRow.SupplierName);
+        Assert.AreEqual(supplier.Address, dbRow.SupplierAddress);
+        Assert.AreEqual(supplier.VatNumber, dbRow.SupplierVatNumber);
+        Assert.AreEqual(supplier.Iban, dbRow.SupplierIban);
+        Assert.AreEqual(client.Name, dbRow.ClientName);
+        Assert.AreEqual(client.Address, dbRow.ClientAddress);
+        Assert.AreEqual(client.VatNumber, dbRow.ClientVatNumber);
+    }
+
+    ExpandoObject DbRow(int id)
+    {
+        using var command = pgDataSource.CreateCommand("SELECT * FROM invoices WHERE id = $1");
+        command.Parameters.AddWithValue(id);
         using var reader = command.ExecuteReader();
         reader.Read();
-        Assert.AreEqual(number, reader["number"]);
-        Assert.AreEqual(date, reader.GetFieldValue<DateOnly>(reader.GetOrdinal("date")));
-        Assert.AreEqual(dueDate, reader.GetFieldValue<DateOnly>(reader.GetOrdinal("due_date")));
-        Assert.AreEqual(vatRate, reader["vat_rate"]);
-        Assert.AreEqual(client.Id, reader["client_id"]);
-        Assert.AreEqual(supplier.Name, reader["supplier_name"]);
-        Assert.AreEqual(supplier.Address, reader["supplier_address"]);
-        Assert.AreEqual(supplier.VatNumber, reader["supplier_vat_number"]);
-        Assert.AreEqual(supplier.Iban, reader["supplier_iban"]);
-        Assert.AreEqual(client.Name, reader["client_name"]);
-        Assert.AreEqual(client.Address, reader["client_address"]);
-        Assert.AreEqual(client.VatNumber, reader["client_vat_number"]);
+
+        dynamic row = new ExpandoObject();
+        row.Number = reader["number"];
+        row.Date = reader.GetFieldValue<DateOnly>(reader.GetOrdinal("date"));
+        row.DueDate = reader.GetFieldValue<DateOnly>(reader.GetOrdinal("due_date"));
+        row.VatRate = reader["vat_rate"];
+        row.ClientId = reader["client_id"];
+        row.SupplierName = reader["supplier_name"];
+        row.SupplierAddress = reader["supplier_address"];
+        row.SupplierVatNumber = reader["supplier_vat_number"];
+        row.SupplierIban = reader["supplier_iban"];
+        row.ClientName = reader["client_name"];
+        row.ClientAddress = reader["client_address"];
+        row.ClientVatNumber = reader["client_vat_number"];
+
+        return row;
     }
 }
