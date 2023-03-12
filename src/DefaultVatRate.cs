2c39e9c038670f00820dadc0471381204eff77e1
Artur Beljajev <artur@intech.ee>
2023-03-12 12:46:03 +0000

    Implement default vat rate quality

--- src/DefaultVatRate.cs
+++ src/DefaultVatRate.cs
@@ -1,9 +1,24 @@
 namespace Intech.Invoice;
 
-// TODO Implement VatRate identity
 sealed class DefaultVatRate : VatRate
 {
     readonly int value;
+
+    public static bool operator ==(DefaultVatRate a, DefaultVatRate b)
+    {
+        if (ReferenceEquals(a, b))
+            return true;
+
+        if ((object)a == null || (object)b == null)
+            return false;
+
+        return a.IntValue() == b.IntValue();
+    }
+
+    public static bool operator !=(DefaultVatRate a, DefaultVatRate b)
+    {
+        return !(a == b);
+    }
 
     public DefaultVatRate(int value)
     {
@@ -18,5 +33,15 @@ sealed class DefaultVatRate : VatRate
     public int VatAmount(int amountWithoutVat)
     {
         return amountWithoutVat * value / 100;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is DefaultVatRate other && value == other.value;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(value);
     }
 }
--- test/DefaultVatRateTest.cs
+++ test/DefaultVatRateTest.cs
@@ -17,4 +17,22 @@ class DefaultVatRateTest
         var stdVatRate = new DefaultVatRate(10);
         Assert.AreEqual(20, stdVatRate.VatAmount(200));
     }
+
+    [Test]
+    public void Equality()
+    {
+        var ten = new DefaultVatRate(10);
+        var twenty = new DefaultVatRate(20);
+
+        Assert.True(ten.Equals(ten));
+#pragma warning disable CS1718 // Comparison made to same variable
+        Assert.True(ten == ten);
+#pragma warning restore CS1718 // Comparison made to same variable
+
+        Assert.False(ten.Equals(twenty));
+        Assert.False(ten == twenty);
+        Assert.True(ten != twenty);
+
+        Assert.AreEqual(HashCode.Combine(ten.IntValue()), ten.GetHashCode());
+    }
 }
