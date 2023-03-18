d73617632040fb3961863a365fe4d0381f63fe2b
Artur Beljajev <artur@intech.ee>
2023-03-18 13:18:15 +0000

    Improve readability

--- src/Clock.cs
+++ src/Clock.cs
@@ -4,22 +4,22 @@ interface Clock
 {
     class Fake : Clock
     {
-        private readonly string currentTime;
+        private readonly string iso8601Now;
         private readonly TimeZoneInfo timeZone;
 
         public Fake() : this("1970-01-01 07:00:00", Timezone.Fake()) { }
 
-        public Fake(string currentTime) : this(currentTime, Timezone.Fake()) { }
+        public Fake(string iso8601Time) : this(iso8601Time, Timezone.Fake()) { }
 
-        public Fake(string currentTime, TimeZoneInfo timeZone)
+        public Fake(string iso8601Time, TimeZoneInfo timeZone)
         {
-            this.currentTime = currentTime;
+            iso8601Now = iso8601Time;
             this.timeZone = timeZone;
         }
 
         public DateTime Now()
         {
-            return DateTime.Parse(currentTime);
+            return DateTime.Parse(iso8601Now);
         }
 
         public DateOnly Today()
