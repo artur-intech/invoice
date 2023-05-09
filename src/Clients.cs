e4479fcbdf09144dd617d8576ba41cf7c029017c
Artur Beljajev <artur@intech.ee>
2023-05-09 11:35:24 +0000

    Do not inherit `Clients` interface from `IEnumerable<Client>`

--- src/Clients.cs
+++ src/Clients.cs
@@ -1,24 +1,12 @@
-using System.Collections;
-
 namespace Intech.Invoice;
 
-interface Clients : IEnumerable<Client>
+interface Clients
 {
     class Fake : Clients
     {
         public Client Add(string name, string address, string vatNumber, string email)
         {
             return new Client.Fake();
-        }
-
-        public IEnumerator GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-
-        IEnumerator<Client> IEnumerable<Client>.GetEnumerator()
-        {
-            throw new NotImplementedException();
         }
     }
 
